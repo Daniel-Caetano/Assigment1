643d5e254e6129f250edae102a014334db0010e1
Daniel Caetano de Souza Ferreira <danielcaetano.sf@gmail.com>
2021-10-11 23:12:14 -0300

    Topico 2 : mudando texto ao clicar no botão

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,42 @@ main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  var _textTitle = "I am A BUTTON?";
+  var _textBody = "I am A BUTTON???";
+  var _textButton = "I am A BUTTON???????";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("I am BUTTON?"),
+          title: Text(_textTitle),
         ),
-        body: Column(
-          children: [
-            Text("I AM BUTTON??"),
-          ],
+        body: Center(
+          child: Column(
+            children: [
+              Text(_textBody),
+              RaisedButton(
+                child: Text(_textButton),
+                textColor: Colors.blue,
+                onPressed: () {
+                  setState(
+                    () {
+                      _textTitle = "NOO!!";
+                      _textBody = "NOOOOO!!";
+                      _textButton = "YES I AM!!!!";
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
